f0bf571606215ad9449ba936b9ca6ba3d489d029
Evonee Uribe <euribe@arbonne.com>
2017-05-20 17:15:32 -0700

    adding php page for google

--- web/index.php
+++ web/index.php
@@ -22,4 +22,9 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/webhook', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('webhook.twig');
+});
+
 $app->run();
