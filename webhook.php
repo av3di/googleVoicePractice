180965f123c70c2ae106fac8ed705368540df1a9
Evonee Uribe <euribe@arbonne.com>
2017-05-20 20:06:19 -0700

    adding php input

--- webhook.php
+++ webhook.php
@@ -4,7 +4,7 @@ header('Content-type: application/json');
 
 // php://input contains raw data, POST can only handle simple key value
 // whereas input can handle more complicated such as nested arrays, etc
-//$body = file_get_contents("php://input");
+$body = file_get_contents("php://input");
 //$webhook = json_decode($body, true);
 
 $response->speech = "Nope, guess again!";
